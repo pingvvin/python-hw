a840031beb34d79bb29ecb9161d4772c0fca42f6
pingvvin <71318818+pingvvin@users.noreply.github.com>
2020-10-22 20:52:40 +0200

    Add files via upload
    file 87 updated

--- lesson87.py
+++ lesson87.py
@@ -4,26 +4,34 @@
 # Проверьте корректность полученного результата.
 
 class Complex:
-    def __init__(self,n):
+    #Complex numbers should be in format '2x+1' where first part should contain variable
+    def __init__(self, n):
         self.n = n
 
     def __add__(self, m):
         piece = self.n.split('+')
         piece2 = m.split('+')
-        print(f'{(int(piece[0][0]) + int(piece2[0][0]))}{piece[0][1]}+{(int(piece2[1][0]) + int(piece2[1][0]))}')
+        print(f'{int(piece[0][:len(piece[0])-1]) + int(piece2[0][:len(piece2[0])-1])}'
+              f'{piece2[0][len(piece2[0])-1:]}+{int(piece[1])+int(piece2[1])}')
+
+    def __sub__(self, m):
+        piece = self.n.split('+')
+        piece2 = m.split('+')
+        print(f'{int(piece[0][:len(piece[0])-1]) - int(piece2[0][:len(piece2[0])-1])}'
+              f'{piece2[0][len(piece2[0])-1:]}+{int(piece[1])-int(piece2[1])}')
 
 
+    def __mul__(self, m):
+        piece = self.n.split('+')
+        piece2 = m.split('+')
+        print(f'{int(piece[1])*int(piece2[1]) - int(piece[0][:len(piece[0])-1]) * int(piece2[0][:len(piece2[0])-1])}'
+              f'{piece2[0][len(piece2[0])-1:]}+{int(piece[1])*int(piece2[0][:len(piece2[0])-1]) + int(piece2[1]) * int(piece[0][:len(piece[0])-1])}')
 
 
+# Не осилил замену плюсиков поэтоми при отрицательных числах будет появляться строка формата 2х+-3 что теоретически можно понять
 
+test = Complex('1x+2')
 
-test = Complex('2x+3')
-
-test + '13x+2'
-
-a = '13x+2'
-
-
-
-'2x+3'
-'3+2x'
+test.__add__('31x+41')
+test.__sub__('31x+41')
+test.__mul__('3x+4')
